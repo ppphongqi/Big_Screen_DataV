334ec3852c0793631b420407acc6634429d30d85
ppphongqi <2503389490@qq.com>
2021-05-20 20:47:38 +0800

    'version2'

--- src/views/Left/index.vue
+++ src/views/Left/index.vue
@@ -55,7 +55,7 @@
     </Card>
     <Card title="排行" >
       <div class="rank">
-        <dv-scroll-board :config="config" style="width:100%;height:93%" />
+        <dv-scroll-board :config="config" style="width:100%;height:100%" headerBGC="rgb(0,0,0)" />
       </div>
     </Card>
   </div>
@@ -75,20 +75,23 @@ export default {
       config: {
         header: ['展品', '观看量', '好评量'],
         data: [
-          ['<span style="color:#37a2da;">行1列1</span>', '行1列2', '行1列3'],
-          ['行2列1', '<span style="color:#32c5e9;">行2列2</span>', '行2列3'],
-          ['行3列1', '行3列2', '<span style="color:#67e0e3;">行3列3</span>'],
-          ['行4列1', '<span style="color:#9fe6b8;">行4列2</span>', '行4列3'],
-          ['<span style="color:#ffdb5c;">行5列1</span>', '行5列2', '行5列3'],
-          ['行6列1', '<span style="color:#ff9f7f;">行6列2</span>', '行6列3'],
-          ['行7列1', '行7列2', '<span style="color:#fb7293;">行7列3</span>'],
-          ['行8列1', '<span style="color:#e062ae;">行8列2</span>', '行8列3'],
-          ['<span style="color:#e690d1;">行9列1</span>', '行9列2', '行9列3'],
-          ['行10列1', '<span style="color:#e7bcf3;">行10列2</span>', '行10列3'],
+          ['中小学教材', '2800', '1900'],
+          ['高校教材', '2700', '1800'],
+          ['职教教材', '2600', '1700'],
+          ['小学教材', '2500', '1600'],
+          ['多媒体', '2400', '1500'],
+          ['小学教材', '2300', '1400'],
+          ['职教教材', '2200', '1300'],
+          ['中小学教材', '2100', '1200'],
+          ['高校教材', '2000', '1100'],
+          ['大学教材', '1900', '1000'],
         ],
         // index: true,
-        columnWidth: [250, 100, 100],
-        align: ['center'],
+        columnWidth: [250],
+        align: ['center', 'center', 'center'],
+        headerBGC: 'rgba( 255,255,255 , 0.1)',
+        oddRowBGC: 'rgb(92,81,138)',
+        evenRowBGC: 'rgb(32,41,83)',
       },
       statisticsList: [
         {
